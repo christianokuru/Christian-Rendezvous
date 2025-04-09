d5f8b84389f55b4a301caf248da466b72d242833
christianokuru <okuruchristian@gmail.com>
2025-04-09 18:59:02 +0100

    Finished the desktop navbar

--- components/custom/ButtonComponent.vue
+++ components/custom/ButtonComponent.vue
@@ -1,8 +1,18 @@
 <script setup>
-const props = defineProps()
+const props = defineProps({
+    text: {
+        type: String,
+        required: true,
+    },
+    icon: {
+        type: String,
+        default: null,
+    },
+})
 </script>
 <template>
-    <div>
-        <h1>Home</h1>
-    </div>
+    <button class="flex items-center space-x-[8px] bg-[#783EAD] py-[12px] px-[24px] rounded-[10px]">
+        <span class="text-[#FFFFFF] font-[400] text-[16px] leading-[100%]">{{text}}</span>
+        <img v-if="icon" :src="icon" class="w-1 h-1"/>
+    </button>
 </template>
--- components/custom/NavbarComponent.vue
+++ components/custom/NavbarComponent.vue
@@ -1,4 +1,5 @@
 <script setup>
+import ButtonComponent from '@/components/custom/ButtonComponent.vue'
 
 const navLinks = [
   { name: 'Discover', to: '/discover' },
@@ -9,17 +10,17 @@ const navLinks = [
 
 </script>
 <template>
-    <nav class="absolute z-10 bg-[#FFFFFF99] rounded-[20px] mx-[64px] mt-[50px] py-[12px] border border-red-500">
-      <div class=" flex items-center justify-between mx-[24px] border border-green-500 space-x-[258.5px]">
+    <nav class="absolute z-10 bg-[#FFFFFF99] rounded-[20px] mx-[64px] mt-[50px] py-[12px]">
+      <div class=" flex items-center justify-between mx-[24px] space-x-[258.5px]">
         <div>
             <div class="font-[400] text-[32px] leading-[100%] text-[#432361]">rendezvous</div>
         </div>
         <!-- Center: Nav Links -->
-        <ul class="flex space-x-6 justify-center flex-1">
+        <ul class="flex space-x-[16px] items-center">
           <li v-for="(link, index) in navLinks" :key="index">
             <nuxt-link
               :to="link.to"
-              class="text-gray-700 hover:text-blue-600 transition-colors duration-300"
+              class="text-[#432361] hover:text-[#43236186] font-[400] text-[16px] leading-[100%]"
             >
               {{ link.name }}
           </nuxt-link>
@@ -27,15 +28,12 @@ const navLinks = [
         </ul>
   
         <!-- Right: Auth Buttons -->
-        <div class="flex items-center space-x-4">
-          <nuxt-link to="/login" class="text-gray-700 hover:text-blue-600 transition-colors mr-[24px]">
+        <div class="flex items-center">
+          <nuxt-link to="/login" class="text-[#432361] font-[400] text-[16px] leading-[100%] hover:text-[#6b389b] p-[8px] mr-[24px]">
             Log In
           </nuxt-link>
-          <nuxt-link
-            to="/signup"
-            class="bg-purple-600 text-white px-4 py-2 rounded-md hover:bg-purple-700 transition-colors"
-          >
-            Sign Up
+          <nuxt-link to="/signup">
+            <button-component text="Sign up" />
           </nuxt-link>
         </div>
         
