0dd2121d0c16c65663ec53290555c0442d44f565
christianokuru <okuruchristian@gmail.com>
2025-04-09 09:21:14 +0100

    Fixed the navbar

--- components/custom/NavbarComponent.vue
+++ components/custom/NavbarComponent.vue
@@ -9,34 +9,34 @@ const navLinks = [
 
 </script>
 <template>
-    <nav class="absolute z-10 w-[1312px]  bg-[#FFFFFF99] rounded-[20px] mx-auto mt-[50px] py-4 border border-red-500">
-      <div class="mx-auto flex items-center justify-between">
+    <nav class="absolute z-10 bg-[#FFFFFF99] rounded-[20px] mx-[64px] mt-[50px] py-[12px] border border-red-500">
+      <div class=" flex items-center justify-between mx-[24px] border border-green-500 space-x-[258.5px]">
         <div>
             <div class="font-[400] text-[32px] leading-[100%] text-[#432361]">rendezvous</div>
         </div>
         <!-- Center: Nav Links -->
         <ul class="flex space-x-6 justify-center flex-1">
           <li v-for="(link, index) in navLinks" :key="index">
-            <NuxtLink
+            <nuxt-link
               :to="link.to"
               class="text-gray-700 hover:text-blue-600 transition-colors duration-300"
             >
               {{ link.name }}
-            </NuxtLink>
+          </nuxt-link>
           </li>
         </ul>
   
         <!-- Right: Auth Buttons -->
         <div class="flex items-center space-x-4">
-          <NuxtLink to="/login" class="text-gray-700 hover:text-blue-600 transition-colors">
+          <nuxt-link to="/login" class="text-gray-700 hover:text-blue-600 transition-colors mr-[24px]">
             Log In
-          </NuxtLink>
-          <NuxtLink
+          </nuxt-link>
+          <nuxt-link
             to="/signup"
             class="bg-purple-600 text-white px-4 py-2 rounded-md hover:bg-purple-700 transition-colors"
           >
             Sign Up
-          </NuxtLink>
+          </nuxt-link>
         </div>
         
       </div>
