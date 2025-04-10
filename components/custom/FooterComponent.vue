93f540f5a861d2e87a8c0e462b3daa3767e8d810
christianokuru <okuruchristian@gmail.com>
2025-04-10 11:51:43 +0100

    Finished the desktop footer

--- components/custom/FooterComponent.vue
+++ components/custom/FooterComponent.vue
@@ -1,8 +1,74 @@
-<script setup>
-
-</script>
+<script setup></script>
 <template>
-    <div>
-        <h1>Footer</h1>
+  <div class="bg-[#432361] text-[#FFFFFF] pb-[120px]">
+    <!-- 1st -->
+    <div class="mx-[64px] flex-1 flex pt-[56px]">
+      <div class="mr-[260px]">
+        <h1 class="font-[400] text-[32px] leading-[100%] mb-[8px]">
+          rendezvous
+        </h1>
+        <p class="font-[400] text-[16px] leading-[100%] w-[332px]">
+          Your Personal Event Sherpa: Curating Awesome, One Click at a Time.
+        </p>
+      </div>
+
+      <div class="flex space-x-[84px] justify-between">
+        <div>
+          <h1 class="font-[400] text-[20px] leading-[100%] mb-[18px]">
+            Feature
+          </h1>
+          <p
+            class="font-[400] text-[16px] leading-[100%] mb-[12px] text-nowrap"
+          >
+            Events discoveries
+          </p>
+          <p class="font-[400] text-[16px] leading-[100%]">Ticketing</p>
+        </div>
+
+        <div>
+          <h1 class="font-[400] text-[20px] leading-[100%] mb-[18px]">
+            Company
+          </h1>
+          <div class="flex flex-col">
+            <nuxt-link
+              to="/"
+              class="font-[400] text-[16px] leading-[100%] mb-[12px]"
+              >About us</nuxt-link
+            >
+            <nuxt-link
+              to="/"
+              class="font-[400] text-[16px] leading-[100%] mb-[12px]"
+              >FAQs</nuxt-link
+            >
+            <nuxt-link
+              to="/"
+              class="font-[400] text-[16px] leading-[100%] mb-[12px]"
+              >Careers</nuxt-link
+            >
+            <nuxt-link
+              to="/"
+              class="font-[400] text-[16px] leading-[100%] mb-[12px]"
+              >Support</nuxt-link
+            >
+          </div>
+        </div>
+
+        <div class="w-[240px]">
+          <h1 class="font-[400] text-[20px] leading-[100%] mb-[18px]">
+            Contact us
+          </h1>
+          <p class="font-[400] text-[16px] leading-[100%] mb-[12px]">
+            info@events.com
+          </p>
+          <p class="font-[400] text-[16px] leading-[100%] mb-[12px]">
+            +234 701 345 6789
+          </p>
+          <p class="font-[400] text-[16px] leading-[100%] mb-[12px]">
+            Race Course, 8/9 Marina, Onikan, Lagos Lagos, 4aa Force Rd, Lagos
+            Island 102273, Lagos
+          </p>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
